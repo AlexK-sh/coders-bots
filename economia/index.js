7e2dae03f2750955cf5e72264de1b6a293785a0f
kredoF <72181870+kredoF@users.noreply.github.com>
2021-02-24 21:57:03 -0500

    Sistema de economia
    Un sistema de economía par tu bot servidor mas información en el  vid https://youtu.be/lp6_fnT88NI

--- economia/index.js
+++ economia/index.js
@@ -1,1 +1,44 @@
-a
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const { Client, MessageEmbed, Collection, Guild} = require("discord.js");
+
+const fs = require("fs");
+const { readdirSync } = require("fs");
+const message = require("./eventos/ready");
+
+var prefix = "!"
+
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./comandos').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles){
+    const command = require(`./comandos/${file}`);
+    client.commands.set(command.name, command);
+}
+
+for (const file of readdirSync(`./eventos/`)){
+    if(file.endsWith(".js")){
+        let fileName = file.substring(0, file.length - 3);
+        let fileContents = require(`./eventos/${file}`);
+    client.on(fileName, fileContents.bind(null, client ));
+    }
+}
+
+client.on("message", async (message) => {
+if(!message.content.startsWith(prefix)) return;
+const args = message.content.slice(prefix.length).trim().split(/ +/g);
+if(message.author.bot)return;
+if(message.channel.type == "dm")return;
+
+const command = args.shift().toLowerCase();
+
+let cmd = client.commands.find((c) => c.name === command || c.alias && c.alias.includes(command));
+
+if(cmd){
+    cmd.execute(client, message , args)
+}
+    
+})
+
+
+client.login("ODEyNzQ4ODQwMjk3NTYyMTUz.YDFRVw.2iUVADFqLTVj1ukG_r6_BG2acwE")
