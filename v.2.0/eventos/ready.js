7032bc7a20d15ad717508d2281454f32085cb9f4
kredoF <72181870+kredoF@users.noreply.github.com>
2021-07-05 20:45:03 -0500

    Fix errs

--- v.2.0/eventos/ready.js
+++ v.2.0/eventos/ready.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = {
 	name: 'ready',//nombre del evento
-async execute(message, client, prefix) { 
+async execute(message, client) { 
 
     console.log("El bot se ha encendido, mnesaje enviado desde el event handler")
 
